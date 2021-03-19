3236212301fb512c644b80ece41b8ba41e9537e3
nykygit <18310180+nykygit@users.noreply.github.com>
2021-03-19 18:50:56 -0500

    Create configure_web1.ps1

---
+++ configure_web1.ps1
@@ -0,0 +1,23 @@
+# Rename the computer to WEB1
+Rename-Computer -NewName "WEB1" -Restart
+
+# Configure Static IP
+New-NetIPAddress –IPAddress 192.168.1.106 -DefaultGateway 192.168.1.1 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
+
+# Configure the DNS - to use DC1
+Set-DNSClientServerAddress –InterfaceIndex (Get-NetAdapter).InterfaceIndex –ServerAddresses 192.168.1.102
+
+# Join the Domain
+Add-Computer -DomainName "LAB.COM" -Restart
+
+# Enter the domain admin creds
+USER = LAB\Administrator
+PASSWORD = <StrongPassword>
+
+# After reboot and switch user to login as the domain account
+USER = LAB\Administrator
+PASSWORD = <StrongPassword>
+
+# Congratulations - you’re now cooking with fire!
+# Install AD Tools to make GUI administration easy - easier than switching screens
+Install-WindowsFeature -Name "RSAT-ADDS"
